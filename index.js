31da77b0edcecd79add42372a491e1fe5d0e5bbc
Nathan-Mez <106163966+Nathan-Mez@users.noreply.github.com>
2022-07-16 13:55:03 +0300

    Password JWT authorization temporarily removed

--- index.js
+++ index.js
@@ -92,7 +92,8 @@ app.get('/movies/director/:Name', passport.authenticate('jwt', { session: false 
 
 
 //Get all users
-app.get('/users', passport.authenticate('jwt', { session: false }), (req, res) => {
+//app.get('/users', passport.authenticate('jwt', { session: false }), (req, res) => {
+app.get('/users', (req, res) => {  
     Users.find()
       .then((users) => {
         res.status(201).json(users);
